1938f0c678cfd2d9f562cf6aca7116251e59acbe
DevX32 <60389339+DevX32@users.noreply.github.com>
2023-09-15 14:21:09 +0000

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -383,7 +383,16 @@ function LockpickDoor(isAdvanced)
     if GetVehicleDoorLockStatus(vehicle) <= 0 then return end
 
     usingAdvanced = isAdvanced
-    Config.LockPickDoorEvent()
+    lib.requestAnimDict('veh@break_in@0h@p_m_one@')
+    TaskPlayAnim(cache.ped, 'veh@break_in@0h@p_m_one@', "low_force_entry_ds", 3.0, 3.0, -1, 16, 0, 0, 0, 0)
+    local success = lib.skillCheck({'easy', 'easy', {areaSize = 60, speedMultiplier = 1}, 'medium'}, {'1', '2', '3', '4'})
+    if success then
+        LockpickFinishCallback(success)
+    else
+        AttemptPoliceAlert('carjack')
+        TriggerServerEvent('hud:server:GainStress', math.random(1, 4))
+        lib.notify({ description = 'You failed to lockpick.', type = 'error' })
+    end
 end
 
 function LockpickFinishCallback(success)
