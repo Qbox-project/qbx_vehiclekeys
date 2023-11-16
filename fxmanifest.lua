5818d271c3c1231f7955e16184038cc12a3dd6f7
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 10:11:53 -0500

    fix: manifest update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,25 @@
 fx_version 'cerulean'
 game 'gta5'
-lua54 'yes'
-description 'https://github.com/Qbox-project/qbx-vehiclekeys'
+
+description 'QBX_VehicleKeys'
+repository 'https://github.com/Qbox-project/qbx_vehiclekeys'
 version '1.0.0'
 
 shared_scripts {
 	'@ox_lib/init.lua',
-	'@qbx_core/import.lua',
+	'@qbx_core/modules/utils.lua',
 	'@qbx_core/shared/locale.lua',
 	'locales/en.lua',
 	'locales/*.lua',
 	'config.lua',
 }
-client_script 'client/main.lua'
+
+client_scripts {
+	'@qbx_core/modules/playerdata.lua',
+	'client/main.lua',
+}
+
 server_script 'server/main.lua'
 
-modules {
-	'qbx_core:playerdata',
-	'qbx_core:utils'
-}
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
