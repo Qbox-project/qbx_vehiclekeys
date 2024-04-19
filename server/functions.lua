b623ac866c24a6c938ace226486cfa00a22865a6
Artur Michalak <36608116+ArturMichalak@users.noreply.github.com>
2024-04-19 17:35:36 +0200

    refactor: Re-organization and rename
    * merge main to pr
    
    * fix: qb-vehiclekeys:client:giveKeys to qb-vehiclekeys:client:GiveKeys in event register
    
    * Refactor: Putting the lockpicking in a separate thread has made the UI smoother

--- server/functions.lua
+++ server/functions.lua
@@ -1,7 +1,7 @@
 --- Checks for the existence of a key.
 ---@param entity number The entity (vehicle) where we check for the existence of a key.
 ---@param citizenid string The CitizenID of the player whose key we check for.
----@return boolean | nil if the player has a key for the vehicle, nil otherwise.
+---@return boolean? `true` if the player has a key for the vehicle, nil otherwise.
 function HasKey(entity, citizenid)
     if not entity or type(entity) ~= 'number' or not citizenid or type(citizenid) ~= 'string' then return end
     local ent = Entity(entity)
@@ -13,7 +13,7 @@ end
 ---@param entity number The entity (vehicle) to which the key is added.
 ---@param citizenid string The CitizenID of the player whose key is being added.
 ---@param doorState number | nil -- Sets the doorState of the vehicle if present
----@return boolean | nil `true` if the key was successfully added, `nil` otherwise.
+---@return boolean? `true` if the key was successfully added, nil otherwise.
 function GiveKey(entity, citizenid, doorState)
     if not entity or type(entity) ~= 'number' or not citizenid or type(citizenid) ~= 'string' then return end
 
@@ -36,7 +36,7 @@ end
 --- Removes a key from the selected vehicle entity and returns a success status.
 ---@param entity number The entity (vehicle) from which the key is removed.
 ---@param citizenid string The CitizenID of the player whose key is being removed.
----@return boolean | nil `true` if the key was successfully removed, `nil` otherwise.
+---@return boolean? `true` if the key was successfully removed, `nil` otherwise.
 function RemoveKey(entity, citizenid)
     if not entity or type(entity) ~= 'number' or not citizenid or type(citizenid) ~= 'string' then
         return
@@ -56,7 +56,7 @@ end
 --- Sets the door state of the vehicle.
 ---@param entity number The entity (vehicle) for which the door state is updated.
 ---@param doorState number The door state number to update.
----@return boolean | nil `true` if the door state was successfully updated, `nil` otherwise.
+---@return boolean? `true` if the door state was successfully updated, `nil` otherwise.
 function SetDoorState(entity, doorState)
     if not entity or type(entity) ~= 'number' or not doorState or type(doorState) ~= 'number' then return end
 
