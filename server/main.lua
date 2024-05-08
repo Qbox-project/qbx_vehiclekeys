1e8f36d4825cdb5dfcdf96afb3d26a223981b8be
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-05-08 15:59:34 +0200

    fix(server): invalid players

--- server/main.lua
+++ server/main.lua
@@ -1,14 +1,20 @@
 -----------------------
 ----   Variables   ----
 -----------------------
+
 local vehicleList = {}
 
 -----------------------
-----   Threads     ----
+----   Functions   ----
 -----------------------
 
-CreateThread(function()
-    local vehicles = MySQL.query.await('SELECT * FROM player_vehicles')
+---Loads a players vehicles to the vehicleList
+---@param src integer
+local function addPlayer(src)
+    local player = exports.qbx_core:GetPlayer(src)
+    if not player then return end
+
+    local vehicles = MySQL.query.await('SELECT * FROM player_vehicles WHERE citizenid = ?', { player.PlayerData.citizenid })
     for i = 1, #vehicles do
         local data = vehicles[i]
         vehicleList[data.plate] = {}
@@ -17,7 +23,50 @@ CreateThread(function()
             vehicleList[data.plate][data.citizenid] = true
         end
     end
-end)
+end
+
+---Removes a players vehicles from the vehicleList
+---@param src integer
+local function removePlayer(src)
+    local player = exports.qbx_core:GetPlayer(src)
+    if not player then return end
+
+    for k, v in pairs(vehicleList) do
+        if v[player.PlayerData.citizenid] then
+            vehicleList[k][player.PlayerData.citizenid] = nil
+        end
+    end
+end
+
+function GiveKeys(id, plate)
+    local player = exports.qbx_core:GetPlayer(id)
+    if not player then return false end
+
+    if not vehicleList[plate] then vehicleList[plate] = {} end
+    vehicleList[plate][player.PlayerData.citizenid] = true
+
+    exports.qbx_core:Notify(id, locale('notify.keys_taken'))
+    TriggerClientEvent('qb-vehiclekeys:client:AddKeys', id, plate)
+
+    return true
+end
+
+exports('GiveKeys', GiveKeys)
+
+function RemoveKeys(id, plate)
+    local player = exports.qbx_core:GetPlayer(id)
+    if not player or not vehicleList[plate] or not vehicleList[plate][player.PlayerData.citizenid] then return false end
+
+    vehicleList[plate][player.PlayerData.citizenid] = nil
+    TriggerClientEvent('qb-vehiclekeys:client:RemoveKeys', id, plate)
+
+    return true
+end
+
+function HasKeys(id, plate)
+    local player = exports.qbx_core:GetPlayer(id)
+    return player and vehicleList[plate] and vehicleList[plate][player.PlayerData.citizenid]
+end
 
 -----------------------
 ---- Server Events ----
@@ -36,12 +85,11 @@ end)
 -- Must already have keys to the vehicle, trigger the event from the server, or pass forcegive paramter as true.
 RegisterNetEvent('qb-vehiclekeys:server:GiveVehicleKeys', function(receiver, plate)
     local giver = source
-
     if HasKeys(giver, plate) then
         exports.qbx_core:Notify(giver, locale("notify.gave_keys"))
         if type(receiver) == 'table' then
-            for _,r in ipairs(receiver) do
-                GiveKeys(receiver[r], plate)
+            for i = 1, receiver do
+                GiveKeys(receiver[i], plate)
             end
         else
             GiveKeys(receiver, plate)
@@ -52,8 +100,7 @@ RegisterNetEvent('qb-vehiclekeys:server:GiveVehicleKeys', function(receiver, pla
 end)
 
 RegisterNetEvent('qb-vehiclekeys:server:AcquireVehicleKeys', function(plate)
-    local src = source
-    GiveKeys(src, plate)
+    GiveKeys(source, plate)
 end)
 
 RegisterNetEvent('qb-vehiclekeys:server:breakLockpick', function(itemName)
@@ -78,38 +125,6 @@ lib.callback.register('qbx-vehiclekeys:server:getVehicleKeys', function(source)
     return keysList
 end)
 
------------------------
-----   Functions   ----
------------------------
-function GiveKeys(id, plate)
-    local citizenid = exports.qbx_core:GetPlayer(id).PlayerData.citizenid
-
-    if not vehicleList[plate] then vehicleList[plate] = {} end
-    vehicleList[plate][citizenid] = true
-
-    exports.qbx_core:Notify(id, locale('notify.keys_taken'))
-    TriggerClientEvent('qb-vehiclekeys:client:AddKeys', id, plate)
-end
-exports('GiveKeys', GiveKeys)
-
-function RemoveKeys(id, plate)
-    local citizenid = exports.qbx_core:GetPlayer(id).PlayerData.citizenid
-
-    if vehicleList[plate] and vehicleList[plate][citizenid] then
-        vehicleList[plate][citizenid] = nil
-    end
-
-    TriggerClientEvent('qb-vehiclekeys:client:RemoveKeys', id, plate)
-end
-
-function HasKeys(id, plate)
-    local citizenid = exports.qbx_core:GetPlayer(id).PlayerData.citizenid
-    if vehicleList[plate] and vehicleList[plate][citizenid] then
-        return true
-    end
-    return false
-end
-
 ---Gives a key to an entity based on the player's CitizenID.
 ---@param id integer The player's ID.
 ---@param netId number The network ID of the entity.
@@ -121,6 +136,7 @@ RegisterNetEvent('qb-vehiclekeys:server:GiveKey', function(id, netId, doorState)
         -- drop player
     end
 end)
+
 exports('GiveKey', GiveKey)
 
 ---Removes a key from an entity based on the player's CitizenID.
@@ -133,6 +149,7 @@ RegisterNetEvent('vehiclekeys:server:RemoveKey', function(id, netId)
         -- drop player
     end
 end)
+
 exports('RemoveKey', RemoveKey)
 
 ---Sets the door state to a desired value.
@@ -146,7 +163,25 @@ RegisterNetEvent('vehiclekeys:server:SetDoorState', function(netId, doorState)
         -- drop player
     end
 end)
+
 exports('SetDoorState', SetDoorState)
+
+AddEventHandler('QBCore:Server:OnPlayerLoaded', function()
+    addPlayer(source --[[@as integer]])
+end)
+
+---@param src integer
+AddEventHandler('QBCore:Server:OnPlayerUnload', function(src)
+    removePlayer(src)
+end)
+
+AddEventHandler('playerDropped', function()
+    removePlayer(source --[[@as integer]])
+end)
+
+-----------------------
+----   Callbacks   ----
+-----------------------
 
 ---Gives a key to an entity based on the target player's CitizenID but only if the owner already has a key.
 ---@param source number ID of the player
@@ -182,4 +217,20 @@ end)
 ---@return boolean
 lib.callback.register('vehiclekeys:server:IsPlayerOwned', function(_, plate)
     return not not vehicleList[plate]
+end)
+
+-----------------------
+----   Threads     ----
+-----------------------
+
+CreateThread(function()
+    local vehicles = MySQL.query.await('SELECT * FROM player_vehicles')
+    for i = 1, #vehicles do
+        local data = vehicles[i]
+        vehicleList[data.plate] = {}
+
+        if data.citizenid then
+            vehicleList[data.plate][data.citizenid] = true
+        end
+    end
 end)
