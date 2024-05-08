1bf112fdb65e40bae1efd166e09f12654b8ad862
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-05-08 15:40:36 +0200

    fix(server/commands): args inconsistenties

--- server/commands.lua
+++ server/commands.lua
@@ -1,72 +1,76 @@
 lib.addCommand('givekeys', {
-    help = locale("addcom.givekeys"),
+    help = locale('addcom.givekeys'),
     params = {
         {
-            name = locale("addcom.givekeys_id"),
-            type = 'number',
-            help = locale("addcom.givekeys_id_help"),
+            name = locale('addcom.givekeys_id'),
+            type = 'playerId',
+            help = locale('addcom.givekeys_id_help'),
             optional = true
         },
         {
-            name = locale("addcom.givekeys_plate"),
+            name = locale('addcom.givekeys_plate'),
             type = 'string',
-            help = locale("addcom.addkeys_plate_help"),
+            help = locale('addcom.addkeys_plate_help'),
             optional = true
         },
     },
     restricted = false,
 }, function (source, args)
-    TriggerClientEvent('qb-vehiclekeys:client:GiveKeys', source, args.id, args.plate)
+    TriggerClientEvent('qb-vehiclekeys:client:GiveKeys', source, args[locale('addcom.givekeys_id')], args[locale('addcom.givekeys_plate')])
 end)
 
 lib.addCommand('addkeys', {
-    help = locale("addcom.addkeys"),
+    help = locale('addcom.addkeys'),
     params = {
         {
-            name = locale("addcom.addkeys_id"),
-            type = 'number',
-            help = locale("addcom.addkeys_id_help"),
+            name = locale('addcom.addkeys_id'),
+            type = 'playerId',
+            help = locale('addcom.addkeys_id_help'),
             optional = true
         },
         {
-            name = locale("addcom.addkeys_plate"),
+            name = locale('addcom.addkeys_plate'),
             type = 'string',
-            help = locale("addcom.addkeys_plate_help"),
+            help = locale('addcom.addkeys_plate_help'),
             optional = true
         },
     },
     restricted = 'group.admin',
 }, function (source, args)
-    if not args.id or not args.plate then
-        exports.qbx_core:Notify(source, locale("notify.fpid"))
+    local id = args[locale('addcom.addkeys_id')]
+    local plate = args[locale('addcom.addkeys_plate')]
+    if not id or not plate then
+        exports.qbx_core:Notify(source, locale('notify.fpid'))
         return
     end
 
-    GiveKeys(args.id, args.plate)
+    GiveKeys(id, plate)
 end)
 
 lib.addCommand('removekeys', {
-    help = locale("addcom.remove_keys"),
+    help = locale('addcom.remove_keys'),
     params = {
         {
-            name = locale("addcom.removekeys_id"),
-            type = 'number',
-            help = locale("addcom.remove_keys_id_help"),
+            name = locale('addcom.removekeys_id'),
+            type = 'playerId',
+            help = locale('addcom.remove_keys_id_help'),
             optional = true
         },
         {
-            name = locale("addcom.removekeys_plate"),
+            name = locale('addcom.removekeys_plate'),
             type = 'string',
-            help = locale("addcom.remove_keys_plate_help"),
+            help = locale('addcom.remove_keys_plate_help'),
             optional = true
         }
     },
     restricted = 'group.admin',
 }, function (source, args)
-    if not args.id or not args.plate then
-        exports.qbx_core:Notify(source, locale("notify.fpid"))
+    local id = args[locale('addcom.removekeys_id')]
+    local plate = args[locale('addcom.removekeys_plate')]
+    if not id or not plate then
+        exports.qbx_core:Notify(source, locale('notify.fpid'))
         return
     end
 
-    RemoveKeys(args.id, args.plate)
+    RemoveKeys(id, plate)
 end)
