d3cf142eab5e316d8bd500446ae914d5f5381f25
HC DEV <36608116+ArturMichalak@users.noreply.github.com>
2024-06-22 20:39:47 +0200

    feat(shared): getIsVehicleOnList as common function

--- shared/functions.lua
+++ shared/functions.lua
@@ -10,28 +10,27 @@ function public.getIsCloseToCoords(coord1, coord2, distance)
     return #(coord1 - coord2) < distance
 end
 
+local function getIsVehicleOnList(vehicle, list)
+    local vehicleHash = GetEntityModel(vehicle)
+    for i = 1, #list do
+        if vehicleHash == joaat(list[i]) then
+            return true
+        end
+    end
+end
+
 ---Checking vehicle on the blacklist.
 ---@param vehicle number The entity number of the vehicle.
 ---@return boolean? `true` if the vehicle is blacklisted, `nil` otherwise.
 function public.getIsVehicleBlacklisted(vehicle)
-    local vehicleHash = GetEntityModel(vehicle)
-    for i = 1, #config.noLockVehicles do
-        if vehicleHash == joaat(config.noLockVehicles[i]) then
-            return true
-        end
-    end
+    return getIsVehicleOnList(vehicle, config.noLockVehicles)
 end
 
 ---Checking vehicle on the immunes list.
 ---@param vehicle any
 ---@return boolean? `true` if the vehicle is immune, `nil` otherwise.
 function public.getIsVehicleImmune(vehicle)
-    local vehicleHash = GetEntityModel(vehicle)
-    for i = 1, #config.immuneVehicles do
-        if vehicleHash == joaat(config.immuneVehicles[i]) then
-            return true
-        end
-    end
+    return getIsVehicleOnList(vehicle, config.immuneVehicles)
 end
 
 return public
