c840e47e1f8185b9fdbc680c4448135a25e2b5aa
ArturMichalak <arturmichalak96@gmail.com>
2024-07-04 21:54:23 +0200

    feat(commands/giveKeys): remove redundant hasKeys checking

--- client/commands.lua
+++ client/commands.lua
@@ -29,9 +29,6 @@ end
 local function giveKeys(id, plate)
     local distance = #(GetEntityCoords(cache.ped) - GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(id))))
     if distance < 3 then
-        if not hasKeys(plate) then
-            return exports.qbx_core:Notify(locale('notify.no_keys'))
-        end
         TriggerServerEvent('qb-vehiclekeys:server:GiveVehicleKeys', id, plate)
         exports.qbx_core:Notify(locale('notify.gave_keys'))
     else
@@ -50,10 +47,6 @@ RegisterNetEvent('qb-vehiclekeys:client:GiveKeys', function(id, plate)
     if id and type(id) == 'number' then                   -- Give keys to specific ID
         giveKeys(id, plate)
     elseif IsPedSittingInVehicle(cache.ped, vehicle) then -- Give keys to everyone in vehicle
-        if not hasKeys(qbx.getVehiclePlate(vehicle)) then
-            return exports.qbx_core:Notify(locale('notify.no_keys'))
-        end
-
         local otherOccupants = getOtherPlayersInVehicle(vehicle)
         for p = 1, #otherOccupants do
             TriggerServerEvent('qb-vehiclekeys:server:GiveVehicleKeys', otherOccupants[p], plate)
