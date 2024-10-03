b0c33ea5868395e1104907cec62659aaeba44c3e
Manason <clanerp@gmail.com>
2024-10-03 01:41:28 -0700

    fix: notify spam

--- bridge/qb/server.lua
+++ bridge/qb/server.lua
@@ -4,7 +4,10 @@ local function giveKeys(source, plate)
     local vehicles = plate and GetVehiclesFromPlate(plate) or {GetVehiclePedIsIn(GetPlayerPed(source), false)}
     local success = nil
     for i = 1, #vehicles do
-        success = success or GiveKeys(source, vehicles[i])
+        success = success or GiveKeys(source, vehicles[i], true)
+    end
+    if success then
+        exports.qbx_core:Notify(source, locale('notify.keys_taken'))
     end
     return success
 end
@@ -14,8 +17,9 @@ CreateQbExport('GiveKeys', giveKeys)
 local function removeKeys(source, plate)
     local vehicles = GetVehiclesFromPlate(plate)
     for i = 1, #vehicles do
-        RemoveKeys(source, vehicles[i])
+        RemoveKeys(source, vehicles[i], true)
     end
+    exports.qbx_core:Notify(source, locale('notify.keys_removed'))
 end
 
 CreateQbExport('RemoveKeys', removeKeys)
