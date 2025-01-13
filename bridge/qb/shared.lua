bf380f191ae458f79e53381c460c4ac082142618
Snowly <visionhyronic@gmail.com>
2025-01-13 16:59:56 +0100

    Update GetVehiclesFromPlate in qb bridge.

--- bridge/qb/shared.lua
+++ bridge/qb/shared.lua
@@ -8,14 +8,12 @@ end
 
 function GetVehiclesFromPlate(plate)
     Wait(500) -- delay needed to give client spawned vehicles time to be known to the server
-    local vehicles = GetAllVehicles()
+    local vehicles = GetGamePool('CVehicle')
     local vehEntityFromPlate = {}
 
     for i = 1, #vehicles do
-        local vehicle = vehicles[i]
-        local vehPlate = qbx.getVehiclePlate(vehicle)
-        if plate == vehPlate or GetVehicleNumberPlateText(vehicle) then
-            vehEntityFromPlate[#vehEntityFromPlate + 1] = vehicle
+        if plate == qbx.getVehiclePlate(vehicles[i]) or GetVehicleNumberPlateText(vehicles[i]) then
+            vehEntityFromPlate[#vehEntityFromPlate + 1] = vehicles[i]
         end
     end
 
