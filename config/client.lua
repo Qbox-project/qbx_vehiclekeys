970d2dd009cbc48f5ab99458981f2366c041ed06
zzjrzz <1981455+zzjrzz@users.noreply.github.com>
2025-06-21 15:55:08 +1000

    Update client.lua

--- config/client.lua
+++ config/client.lua
@@ -24,7 +24,7 @@ local hardLockpickSkillCheck = {
 }
 
 return {
-    vehicleMaximumLockingDistance = 5.0, -- Minimum distance for vehicle locking
+    vehicleMaximumLockingDistance = 5.0, -- Maximum distance for vehicle locking
     getKeysWhenEngineIsRunning = true, -- when enabled, gives keys to a player who doesn't have them if they enter the driver seat when the engine is running
     keepEngineOnWhenAbandoned = true, -- when enabled, keeps a vehicle's engine running after exiting
 
