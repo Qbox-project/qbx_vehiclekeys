9ec731b300a3a5070642b420462d07b2dd074f58
Martinator9001 <90502234+Martinator9001@users.noreply.github.com>
2025-08-19 11:19:07 +0300

    Add givekeys function when entering driver seat unconditionally
    (well, not unconditionally, depends on config ;)

--- client/main.lua
+++ client/main.lua
@@ -48,6 +48,9 @@ local function onEnteringDriverSeat()
             Wait(0)
         end
     end)
+    if(config.getKeysWhenEntering) then
+        TriggerServerEvent('qbx_vehiclekeys:server:tookKeys', VehToNet(vehicle))
+    end
     local vehicleConfig = GetVehicleConfig(vehicle)
     if vehicleConfig.shared then return end
 
